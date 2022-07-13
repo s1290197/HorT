9bdb3e57264ae6eef7d192dfee2e824dfdfbd861
s1290197 <s1290197@u-aizu.ac.jp>
2022-07-14 00:10:50 +0900

    Victory message added

--- headtail.java
+++ headtail.java
@@ -19,5 +19,7 @@ public class headtail{
             }
         }
         System.out.println("Heads: "+head+","+" Tails: "+tail);
+        if(head > tail) System.out.println("You won");
+        else System.out.println("You lost");
     }
 }
